b2e3b4f39d3496139edd719138cf17b11d216c82
Samvel Minasyan <93665133+Levmas25@users.noreply.github.com>
2025-03-16 14:05:33 +0300

    Group number

---
+++ container_with_most_water.py
@@ -0,0 +1,25 @@
+# M3O-232Б-23
+
+def maxArea(height: list[int]) -> int:
+    left = 0
+    right = len(height) - 1
+    area = 0
+
+    while left < right:
+
+        area = max(area, min(height[left], height[right])*(right-left))
+
+        if height[right] > height[left]:
+            left += 1
+            continue
+
+        if height[left] >= height[right]:
+            right -= 1
+
+    return area
+
+
+print('\n')
+print(maxArea([1,8,6,2,5,4,8,3,7]))
+print(maxArea([1, 1]))
+print(maxArea([1,2,4,3]))
